f90676f1d7be8654946af76a10d951de70e19f2e
Natusick <natallia.daniluk@gmail.com>
2022-10-01 00:21:16 +0300

    app

--- myapp/app.js
+++ myapp/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 
+const contacts = require("./contacts.json");
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('<h2>Hello World!</h2>');
 });
+
+app.get('/contacts', (req, res) => {
+  res.json(contacts)
+})
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
