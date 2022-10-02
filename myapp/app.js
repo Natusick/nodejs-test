88d5842ade8e6ca23db105f0156383486ff946ee
Natusick <natallia.daniluk@gmail.com>
2022-10-02 15:09:17 +0300

    vvv-03

--- myapp/app.js
+++ myapp/app.js
@@ -14,3 +14,7 @@ app.get('/contacts', (req, res) => {
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
